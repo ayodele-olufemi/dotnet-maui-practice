859a96449ea06c0dfdc3bdd6f615aa638e466751
Ayodele Olufemi <olufemiae@gmail.com>
2022-06-01 00:47:33 -0500

    added monkeyService

--- Part 2 - MVVM/MonkeyFinder/Services/MonkeyService.cs
+++ Part 2 - MVVM/MonkeyFinder/Services/MonkeyService.cs
@@ -1,5 +1,42 @@
-﻿namespace MonkeyFinder.Services;
+﻿using System.Net.Http.Json;
+
+namespace MonkeyFinder.Services;
 
 public class MonkeyService
 {
+    HttpClient httpClient;
+
+    public MonkeyService()
+    {
+        httpClient = new HttpClient();
+    }
+
+    List<Monkey> monkeyList = new();
+    public async Task<List<Monkey>> GetMonkeys()
+    {
+        if (monkeyList?.Count > 0)
+            return monkeyList;
+
+        var url = "https://montemagno.com/monkeys.json";
+
+        var response = await httpClient.GetAsync(url);
+
+        if (response.IsSuccessStatusCode)
+        {
+            monkeyList = await response.Content.ReadFromJsonAsync<List<Monkey>>();
+        }
+
+        //If you want to read from text or json file (from Resources/Raw folder), use the code below
+
+        /*
+         * 
+        using var stream = await FileSystem.OpenAppPackageFileAsync("monkeydata.json");
+        using var reader = new StreamReader(stream);
+        var contents = await reader.ReadToEndAsync();
+        monkeyList = JsonSerializer.Deserialize<List<Monkey>>(contents);
+
+        */
+
+        return monkeyList;
+    }
 }
