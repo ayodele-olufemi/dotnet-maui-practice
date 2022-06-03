33b9c31df653976d416905b005291718379bb678
Ayodele Olufemi <olufemiae@gmail.com>
2022-06-02 23:32:08 -0500

    finish MVVM

--- Part 2 - MVVM/MonkeyFinder/ViewModel/MonkeysViewModel.cs
+++ Part 2 - MVVM/MonkeyFinder/ViewModel/MonkeysViewModel.cs
@@ -1,5 +1,44 @@
-﻿namespace MonkeyFinder.ViewModel;
+﻿using MonkeyFinder.Services;
+
+namespace MonkeyFinder.ViewModel;
 
 public partial class MonkeysViewModel : BaseViewModel
 {
+    MonkeyService monkeyService;
+    public ObservableCollection<Monkey> Monkeys { get; } = new();
+
+    public MonkeysViewModel(MonkeyService monkeyService)
+    {
+        Title = "Monkey Finder";
+        this.monkeyService = monkeyService;
+    }
+
+    [ICommand]
+    async Task GetMonkeysAsync()
+    {
+        if (IsBusy)
+            return;
+
+        try
+        {
+            IsBusy = true;
+            var monkeys = await monkeyService.GetMonkeys();
+
+            if (monkeys.Count != 0)
+                Monkeys.Clear();
+            
+            // Will raise an event the number of times a monkey is added. May be a problem when adding large number of monkeys
+            foreach (var monkey in monkeys)
+                Monkeys.Add(monkey);
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine(ex);
+            await Shell.Current.DisplayAlert("Error!", $"Unable to get monkeys: {ex.Message}", "OK");
+        }
+        finally
+        {
+            IsBusy = false;
+        }
+    }
 }
